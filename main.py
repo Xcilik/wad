709c67f262d186639784b1b555a316ecaf5ffa24
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-03-07 00:04:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,14 @@ BOT_TOKEN = "6976707406:AAE5RJn-W-H1NKGvkiu0EG3zH88LBiQYsN8"
 app = Client("video_forward_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Fungsi untuk meneruskan video dari satu grup ke grup lainnya
-async def forward_video(client, message):
-    if message.video:
-        try:
-            await client.forward_messages(chat_id=-1002089478686, from_chat_id=message.chat.id, message_ids=message.message_id)
-            await asyncio.sleep(1)
-        except Exception as e:
-            await message.reply(str(e))
+
 
 # Handler untuk menerima pesan video dan meneruskannya 80
-@app.on_message(filters.command("gas"))
+@app.on_message(filters.command("gas") & filters.video)
 async def handle_video(client, message):
     await message.reply("gas")
-    await forward_video(client, message)
+    await message.fordward(-1002089478686)
+    await asyncio.sleep(1)
 
 # Menjalankan bot
 app.run()

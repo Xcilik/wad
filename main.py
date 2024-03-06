813a9ad0ec2080c9a4ff79bd2b5c488b8f23464e
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-03-07 00:30:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ data = [12342, 12092, 12090, 12089, 12088, 12087, 12039, 12038, 12037, 12036, 12
 async def forward_videos(client, message):
     # Ganti "nama_grup_tujuan" dengan nama atau ID grup tujuan
     for id in data:
-        await app.forward_messages(-1002089478686, -1001868635501, id)
+        await app.copy_messages(-1002089478686, -1001868635501, id)
         await asyncio.sleep(1)
 # Menjalankan bot
 app.run()
